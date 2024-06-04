2d0333d24fbd5d8699b1c4ccf0ff9080bd8da505
Frinney <59091487+Frinney@users.noreply.github.com>
2024-06-04 23:19:23 +0300

    Delete BeGrateful-main directory

--- BeGrateful-main/main.js
+++
@@ -1,7 +0,0 @@
-import { setupHeader } from './components/header/header.js';
-import { setupFooter } from './components/footer/footer.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  setupHeader();
-  setupFooter();
-});
